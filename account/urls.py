9773d9b4c726acc4574b67f1d669a2f134c738be
Martins Madueke <maduekemartins6@gmail.com>
2024-06-18 13:01:49 +0100

    login and register view

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,29 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as auth_views
+
+# from users.forms import CustomPasswordResetForm
+
 urlpatterns = [
-    path("", views.loginUsers, name="login"),
-    path("logout/", views.loginUsers, name="logout")
+    path("basic/", views.basic),
+    
+    path("home", views.home, name ="home"),
+    path("", views.loginPage, name ="login"),
+    path("logout/", views.logoutUser, name ="logout"),
+    path("register/", views.register, name ="register"),
+    path("lobby/", views.lobby, name ="lobby"),
+    path("access-denied/", views.accessDenied, name ="access-denied"),
+    path("404/", views.pageNotFound, name ="404"),
+    
+    path("reset-password", auth_views.PasswordResetView.as_view(template_name="reset_password.html"), name="reset_password"),
+    path("reset-password-change", views.requestPasswordStudent, name="reset_password_student"),
+    path("reset-password-done/", auth_views.PasswordResetDoneView.as_view(template_name="reset_password_done.html"), name="password_reset_done"),
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="reset_password_confirm.html"), name="password_reset_confirm"),
+    path("reset-password-complete/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    
+    path('password_change/', auth_views.PasswordChangeView.as_view(),name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    
+    
 ]
