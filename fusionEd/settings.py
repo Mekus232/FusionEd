3042baeae3a91f38e31dcd7da4ce5dbcb686228b
Martins Madueke <maduekemartins6@gmail.com>
2024-06-19 05:40:02 +0100

    11112

--- fusionEd/settings.py
+++ fusionEd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kgvaeizl-)+8as5g7y(suz-h05*@p4w6rb8+tqu)2a%)cx$0@s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "192.168.43.196", "localhost",'stimulating-key-spotless-statement-production.pipeops.app']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -62,7 +62,9 @@ MIDDLEWARE = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://stimulating-key-spotless-statement-production.pipeops.app"
+    "https://stimulating-key-spotless-statement-production.pipeops.app",
+    "https://humorous-humor-spotless-statement-production.pipeops.app/"
+    
 ]
 
 
